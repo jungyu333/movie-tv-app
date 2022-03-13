09f30685bcc4b2722f573dbe625597e818132729
jungyu <jungyu@naver.com>
2022-03-13 16:50:57 +0900

    Updates

--- src/utils.ts
+++
@@ -1,7 +0,0 @@
-export function makeImagePath(path: string, option?: string) {
-  return `https://image.tmdb.org/t/p/${option ? option : "original"}/${path}`;
-}
-
-export function makeVideoPath(path: string) {
-  return `https://www.youtube.com/watch?v=${path}`;
-}
